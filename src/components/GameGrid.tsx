6860e021f1e44400372dc69ba932d2f475970525
Makutu <brightdreamfuture6@gmail.com>
2023-10-23 16:33:58 +0000

    built game cards to hold our data

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,22 @@
+import { SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import { GameCard } from "./GameCard";
 
 export const GameGrid = () => {
   const { games, error } = useGames();
   return (
     <>
       <div>{error}</div>
-      {games.map((game) => (
-        <li key={game.id}>{game.name}</li>
-      ))}
+      <SimpleGrid
+        padding={5}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        overflow="hidden"
+      >
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
     </>
   );
 };

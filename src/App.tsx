fde9fad69b0ad88d104048f50755646f7f1f9613
Makutu <brightdreamfuture6@gmail.com>
2023-10-24 18:41:52 +0000

    displayed genres

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ const App = () => {
     <Grid
       paddingX={5}
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <Navbar />
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,23 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import { getCroppedImageUrl } from "../services/getCroppedImageUrl";
 
 export const GenreList = () => {
   const { data } = useGenres();
   return (
-    <>
+    <List>
       {data.map((genre) => (
-        <div>{genre.name}</div>
+        <ListItem paddingY="8px" key={genre.id}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </>
+    </List>
   );
 };
